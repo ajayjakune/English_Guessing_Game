cb1829ef414c137489e50f709e4656d90f9cbb15
ajayjakune <ajayjakune@users.noreply.github.com>
2020-08-12 22:14:04 +0530

    synonyms activity done

--- EnglishGuessingGame/app/src/main/java/asks/dev/englishguessinggame/MainActivity.java
+++ EnglishGuessingGame/app/src/main/java/asks/dev/englishguessinggame/MainActivity.java
@@ -2,7 +2,10 @@ package asks.dev.englishguessinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start_game = findViewById(R.id.start_game);
+        start_game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, rough.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- EnglishGuessingGame/app/src/main/java/asks/dev/englishguessinggame/rough.java
+++ EnglishGuessingGame/app/src/main/java/asks/dev/englishguessinggame/rough.java
@@ -4,17 +4,22 @@ package asks.dev.englishguessinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
+
 
 
 public class rough extends AppCompatActivity {
@@ -28,6 +33,15 @@ public class rough extends AppCompatActivity {
     int choice;
     DictionaryData dictionaryData;
     String word,option1,option2,option3,option4;
+    TextView que,score;
+    TextView op1,op2,op3,op4;
+    RelativeLayout relativeLayoutForScore;
+    LinearLayout.LayoutParams layoutParams;
+    RelativeLayout.LayoutParams relativeLayoutParams;
+    ImageView[] live;
+    static int count;
+    TextView actualScore;
+    int updateScore;
 
 
     @Override
@@ -40,7 +54,15 @@ public class rough extends AppCompatActivity {
         LL = findViewById(R.id.instruction_layout);
         queHolder = findViewById(R.id.question_holder);
         sv = findViewById(R.id.scroller_view);
+        updateScore = 0;
+        actualScore = findViewById(R.id.actual_score);
+        live = new ImageView[3];
+        live[0] = findViewById(R.id.lives1);
+        live[1] = findViewById(R.id.lives2);
+        live[2] = findViewById(R.id.lives3);
         rm = new Random();
+        count = 3;
+
         try {
             dictionaryData = DictionaryData.getInstance(getApplicationContext());
             dictionaryData.open();
@@ -62,12 +84,23 @@ public class rough extends AppCompatActivity {
 
         next.setClickable(false);
         next.setBackgroundColor(getResources().getColor(R.color.blackish));
-        TextView que = new TextView(this);
-        final TextView op1,op2,op3,op4;
+
+        score = new TextView(this);
+        relativeLayoutForScore = new RelativeLayout(this);
+        relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                                                                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        relativeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        que = new TextView(this);
         op1 = new TextView(this);
         op2 = new TextView(this);
         op3 = new TextView(this);
         op4 = new TextView(this);
+
+
+        layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                                                        LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0,24,0,24);
 
         try {
             int randomNum = rm.nextInt(7127) + 1;
@@ -83,64 +116,75 @@ public class rough extends AppCompatActivity {
             Toast.makeText(this, "" + e.toString(), Toast.LENGTH_SHORT).show();
         }
 
-        que.setPadding(250,120,250,120);
-        que.setBackground(getDrawable(R.drawable.border));
-        que.setText(word);
-        que.setTextSize(30);
-        que.setTextColor(getResources().getColor(R.color.white));
+
+        if(word.length()>14){
+            que.setPadding(50, 100, 50, 100);
+        } else if(word.length()>8) {
+            que.setPadding(100, 100, 100, 100);
+        }else{
+            que.setPadding(200, 100, 200, 100);
+        }
+        que.setBackground(getDrawable(R.drawable.question_background));
+        que.setText("Q. "+word);
+        que.setTextSize(25);
+        que.setTextColor(getResources().getColor(R.color.black));
+        queHolder.addView(que,layoutParams);
 
         op1.setPadding(20,20,20,20);
-        op1.setBackground(getDrawable(R.drawable.border));
-        op1.setText(option1);
-        op1.setTextSize(30);
-        op1.setTextColor(getResources().getColor(R.color.white));
+        op1.setBackground(getDrawable(R.drawable.options_background_white));
+        op1.setTextSize(20);
+        op1.setTextColor(getResources().getColor(R.color.black));
 
         op2.setPadding(20,20,20,20);
-        op2.setBackground(getDrawable(R.drawable.border));
-        op2.setText(option2);
-        op2.setTextSize(30);
-        op2.setTextColor(getResources().getColor(R.color.white));
+        op2.setBackground(getDrawable(R.drawable.options_background_white));
+        op2.setTextSize(20);
+        op2.setTextColor(getResources().getColor(R.color.black));
 
         op3.setPadding(20,20,20,20);
-        op3.setBackground(getDrawable(R.drawable.border));
-        op3.setText(option3);
-        op3.setTextSize(30);
-        op3.setTextColor(getResources().getColor(R.color.white));
+        op3.setBackground(getDrawable(R.drawable.options_background_white));
+        op3.setTextSize(20);
+        op3.setTextColor(getResources().getColor(R.color.black));
 
         op4.setPadding(20,20,20,20);
-        op4.setBackground(getDrawable(R.drawable.border));
-        //LinearLayout ll = new LinearLayout(this);
-        op4.setText(option4);
-        op4.setTextSize(30);
-        op4.setTextColor(getResources().getColor(R.color.white));
-
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                                                                                LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(0,24,0,24);
+        op4.setBackground(getDrawable(R.drawable.options_background_white));
+        op4.setTextSize(20);
+        op4.setTextColor(getResources().getColor(R.color.black));
 
 
         choice = rm.nextInt(4) + 1;
         switch(choice) {
 
-            case 1: queHolder.addView(que,layoutParams);
+            case 1: op1.setText("A. "+option1);
+                    op2.setText("B. "+option2);
+                    op3.setText("C. "+option3);
+                    op4.setText("D. "+option4);
                     queHolder.addView(op1,layoutParams);
                     queHolder.addView(op2,layoutParams);
                     queHolder.addView(op3,layoutParams);
                     queHolder.addView(op4,layoutParams);
                 break;
-            case 2: queHolder.addView(que,layoutParams);
+            case 2: op1.setText("B. "+option1);
+                    op2.setText("A. "+option2);
+                    op3.setText("C. "+option3);
+                    op4.setText("D. "+option4);
                     queHolder.addView(op2,layoutParams);
                     queHolder.addView(op1,layoutParams);
                     queHolder.addView(op3,layoutParams);
                     queHolder.addView(op4,layoutParams);
                 break;
-            case 3: queHolder.addView(que,layoutParams);
+            case 3: op1.setText("C. "+option1);
+                    op2.setText("B. "+option2);
+                    op3.setText("A. "+option3);
+                    op4.setText("D. "+option4);
                     queHolder.addView(op3,layoutParams);
                     queHolder.addView(op2,layoutParams);
                     queHolder.addView(op1,layoutParams);
                     queHolder.addView(op4,layoutParams);
                 break;
-            case 4: queHolder.addView(que,layoutParams);
+            case 4: op1.setText("D. "+option1);
+                    op2.setText("B. "+option2);
+                    op3.setText("C. "+option3);
+                    op4.setText("A. "+option4);
                     queHolder.addView(op4,layoutParams);
                     queHolder.addView(op2,layoutParams);
                     queHolder.addView(op3,layoutParams);
@@ -153,45 +197,32 @@ public class rough extends AppCompatActivity {
         op1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op1.setTextColor(getResources().getColor(R.color.green));
-                op2.setClickable(false);
-                op3.setClickable(false);
-                op4.setClickable(false);
-                next.setClickable(true);
-                next.setBackground(getDrawable(R.drawable.rounded_button));
+                op1.setTextColor(getResources().getColor(R.color.white));
+                correctAnswer();
             }
         });
         op2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op2.setTextColor(getResources().getColor(R.color.red));
-                op1.setClickable(false);
-                op3.setClickable(false);
-                op4.setClickable(false);
-                next.setClickable(true);
-                next.setBackground(getDrawable(R.drawable.rounded_button));
+                op2.setTextColor(getResources().getColor(R.color.white));
+                op2.setBackground(getDrawable(R.drawable.background_red));
+                wrongAnswer();
             }
         });
         op3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op3.setTextColor(getResources().getColor(R.color.red));
-                op1.setClickable(false);
-                op2.setClickable(false);
-                op4.setClickable(false);
-                next.setClickable(true);
-                next.setBackground(getDrawable(R.drawable.rounded_button));
+                op3.setTextColor(getResources().getColor(R.color.white));
+                op3.setBackground(getDrawable(R.drawable.background_red));
+                wrongAnswer();
             }
         });
         op4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                op4.setTextColor(getResources().getColor(R.color.red));
-                op1.setClickable(false);
-                op2.setClickable(false);
-                op3.setClickable(false);
-                next.setClickable(true);
-                next.setBackground(getDrawable(R.drawable.rounded_button));
+                op4.setTextColor(getResources().getColor(R.color.white));
+                op4.setBackground(getDrawable(R.drawable.background_red));
+                wrongAnswer();
             }
         });
 
@@ -201,6 +232,85 @@ public class rough extends AppCompatActivity {
                 sv.fullScroll(View.FOCUS_DOWN);
             }
         });
+
+    }
+
+    private void correctAnswer(){
+        op1.setBackground(getDrawable(R.drawable.background_green));
+        op1.setClickable(false);
+        op2.setClickable(false);
+        op3.setClickable(false);
+        op4.setClickable(false);
+        next.setClickable(true);
+
+        score.setBackground(getDrawable(R.drawable.correct_score_background));
+        score.setText("10");
+        score.setTypeface(null, Typeface.BOLD);
+        score.setPadding(100,20,100,20);
+        score.setTextColor(getResources().getColor(R.color.black));
+        relativeLayoutForScore.addView(score,relativeLayoutParams);
+
+        queHolder.addView(relativeLayoutForScore,layoutParams);
+
+        next.setBackground(getDrawable(R.drawable.rounded_button));
+
+        sv.post(new Runnable() {
+            @Override
+            public void run() {
+                sv.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
+        updateScore += 10;
+        if(updateScore<100){
+            actualScore.setText("0"+updateScore);
+        }else{
+            actualScore.setText(""+updateScore);
+        }
+    }
+
+    private void wrongAnswer(){
+        op1.setBackground(getDrawable(R.drawable.background_green));
+        op1.setClickable(false);
+        op2.setClickable(false);
+        op3.setClickable(false);
+        op4.setClickable(false);
+        next.setClickable(true);
+
+        score.setBackground(getDrawable(R.drawable.wrong_score_background));
+        score.setText("00");
+        score.setTypeface(null, Typeface.BOLD);
+        score.setPadding(100,20,100,20);
+        score.setTextColor(getResources().getColor(R.color.black));
+        relativeLayoutForScore.addView(score,relativeLayoutParams);
+
+        queHolder.addView(relativeLayoutForScore,layoutParams);
+
+        next.setBackground(getDrawable(R.drawable.rounded_button));
+
+        sv.post(new Runnable() {
+            @Override
+            public void run() {
+                sv.fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
+        live[--count].setVisibility(View.INVISIBLE);
+
+        if(count==0){
+            next.setClickable(false);
+            next.setBackgroundColor(getResources().getColor(R.color.blackish));
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(rough.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },3000);
+
+        }
     }
 
 }
